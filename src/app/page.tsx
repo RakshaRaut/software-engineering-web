68c4af1e7e1dcf9dbbdeeac9dab06ee7a84cc9a3
Raksha Raut <raksharaut174@gmail.com>
2024-07-27 14:49:02 +0545

    add product card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import Image from "next/image";
-
+import ProductCard from "@/components/ProductCard";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      This is the landing page
+      <ProductCard
+        avatarText="P"
+        title="Product Name"
+        subheader="Status - 01/01/2019"
+        image="https://example.com/image.jpg"
+        description="This is a brief description of the product that has been configured in the catalog."
+        details={[
+          "Product Type: Simple",
+          "Smartphone Brand X",
+          "Characteristics Camera : 12 Mbps",
+          "Relationships",
+          "Services:",
+        ]}
+      />
     </main>
   );
 }
